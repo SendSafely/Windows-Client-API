ca699ac5cb10a07c13936188a63e367bee3f077c
SendSafely <github@sendsafely.com>
2018-08-31 15:03:40 -0400

    Updates to ParseLinksUtility
    Adds support for hyphinated host names in ParseLinksUtility
    Exposes ParseLinksUtility as public
    
    Original feature request/bug report can be found here: https://github.com/SendSafely/Windows-Client-API/pull/2

--- SendsafelyAPI/Utilities/ParseLinksUtility.cs
+++ SendsafelyAPI/Utilities/ParseLinksUtility.cs
@@ -5,10 +5,17 @@ using System.Text.RegularExpressions;
 
 namespace SendSafely.Utilities
 {
-    class ParseLinksUtility
+    /// <summary>
+    /// Utility for extracting a list of valid SendSafely package links from text input.
+    /// </summary>
+    public class ParseLinksUtility
     {
-        private String REGEX = "(https:\\/\\/[a-zA-Z\\.]+\\/receive\\/\\?[A-Za-z0-9&=\\-]+packageCode=[A-Za-z0-9\\-_]+#keyCode=[A-Za-z0-9\\-_]+)";
+        private String REGEX = "(https:\\/\\/[a-zA-Z\\.-]+\\/receive\\/\\?[A-Za-z0-9&=\\-]+packageCode=[A-Za-z0-9\\-_]+#keyCode=[A-Za-z0-9\\-_]+)";
 
+        /// <summary>
+        /// Extract a list of valid SendSafely package links from text.
+        /// </summary>
+        /// <param name="text"> The text input that you want to check for package links.</param>
         public List<String> Parse(String text)
         {
             List<String> links = new List<String>();
@@ -21,10 +28,10 @@ namespace SendSafely.Utilities
                 {
                     links.Add(match.Value);
                 }
-                   
+
             }
 
-		    return links;
+            return links;
         }
     }
 }
